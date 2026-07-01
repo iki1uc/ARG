98ae1caac34610985b064eb50f18fa41a8f75749
iki1uc <entertrainer@web.de>
2026-07-01 18:10:59 +0200

    Add TMP_LIVE function to handle multiple ARG calls

--- arg-live.js
+++ arg-live.js
@@ -1,1 +1,13 @@
+import { ARG } from "./arg-core.js";
+
+export function TMP_LIVE(term) {
+  return {
+    wiki: ARG(term, "wiki", true),
+    encyclopedia: ARG(term, "encyclopedia", true),
+    argmedia: ARG(term, "argmedia", true),
+    ist: ARG(term, "ist", true),
+    soll: ARG(term, "soll", true),
+    evo: ARG(term, "evo", true)
+  };
+}
 
